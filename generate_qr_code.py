7a73f34fb6560c2d9d5dac63862f8919bbba630e
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-03 20:12:55 +0300

    new commit

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -1,21 +1,23 @@
 import qrcode
 from PIL import Image, ImageDraw
 
+large_url = 'https://github.com/SimplyProgYT'
+
 qr = qrcode.QRCode(
-    version=1,
+    version=10, 
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
     border=4,
 )
+qr.add_data(large_url)
 qr.make(fit=True)
 
 matrix = qr.get_matrix()
 size = len(matrix)
 
-img = Image.new('RGB', (size * 10, size * 10), (255, 255, 255))  
+img = Image.new('RGB', (size * 10, size * 10), (255, 255, 255))
 draw = ImageDraw.Draw(img)
-
-fill_color = (0, 102, 204)  
+fill_color = (0, 102, 204)
 
 for y in range(size):
     for x in range(size):
@@ -25,4 +27,5 @@ for y in range(size):
 border_size = 2
 draw.rectangle([0, 0, img.width, img.height], outline=(0, 0, 0), width=border_size)
 
-img.save('custom_qr_code.png')
+img.save('qr_code.png')
+
